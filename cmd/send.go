6504acc09ec9bf8e0b86484f07c7d9cad4e3c400
ruvido <ruvido@gmail.com>
2024-03-23 13:37:06 +0100

    auto-push

--- cmd/send.go
+++ cmd/send.go
@@ -22,11 +22,19 @@ var sendCmd = &cobra.Command{
 	Use:   "send",
 	// Aliases: []string{"l", "lis", "lst"},
 	Short:  "Send a markdown email to a Pocketbase Collection (including filters)",
-	Args:  cobra.ExactArgs(1),
+	// Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		collectionName := args[0]
-		fmt.Println(collectionName)
-		people := admin.CollectionRecords(collectionName, sendSearchFilter) 
+		var people []admin.User
+		if len(args) > 0 {
+			collectionName := args[0]
+			fmt.Println(collectionName)
+			people = admin.CollectionRecords(collectionName, sendSearchFilter) 
+			fmt.Println(people)
+		} else {
+			fmt.Println("test")
+			people = append(people, admin.User{Email: "ruvido@5p2p.it"})
+			fmt.Println(people[0].Email)
+		}
 		letter.SendEmails( people, markdownFilename)
 
 		// template     := viper.GetString("general.template")
